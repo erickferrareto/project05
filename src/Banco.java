601d01e5bb8fa48b0f0b46a036ab7da6ed79d3fd
Erick Ferrareto <erickskate38@gmail.com>
2022-02-03 23:07:15 -0300

    Start a new exercise

--- src/Banco.java
+++ src/Banco.java
@@ -1,12 +1,21 @@
-/*public class Banco {
+public class Banco {
     public static void main(String[] args) {
-        Conta cont1 = new Conta();
-        cont1.conta = 111;
-        cont1.tipo = "CC";
-        cont1.setDono("Erick");
-        cont1.setSaldo(0);
-        cont1.setStatus(true);
+        Conta c1 = new Conta();
+        c1.setConta(111);
+        c1.setDono("Erick");
+        c1.abrirConta("CC");
+        c1.depositar(300);
+        c1.sacar(349);
 
+        Conta c2 = new Conta();
+        c2.setConta(222);
+        c2.setDono("Tayna");
+        c2.abrirConta("CP");
+        c2.depositar(500);
+        c2.sacar(100);
+
+        c1.estadoAtual();
+        System.out.println("============================");
+        c2.estadoAtual();
     }
 }
-*/
--- src/Conta.java
+++ src/Conta.java
@@ -1,11 +1,78 @@
-/*public class Conta {
-
+public class Conta {
+    // Atributos
     public int conta;
     protected String tipo;
     private String dono;
     private int saldo;
     private boolean status;
 
+    //Métodos
+    public void estadoAtual(){
+        System.out.println("Conta: " + this.getConta());
+        System.out.println("Tipo: " + this.getStatus());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Status: " + this.getStatus());
+    }
+    public void abrirConta(String t){
+        setTipo(t);
+        setStatus(true);
+        if(t.equals("CC")){
+            setSaldo(50);
+        } else if(t.equals("CP")){
+            setSaldo(150);
+        }
+    }
+    public void fecharConta(int s){
+        if (saldo > 0){
+            System.out.println("Conta com dinheiro");
+        } else if (saldo < 0 ){
+            System.out.println("Conta com saldo negativo");
+        } else {
+            status = false;
+        }
+
+    }
+    public void depositar(int s){
+        if (getStatus()){
+            saldo = saldo + s;
+        } else {
+            System.out.println("Conta não aberta");
+        }
+    }
+    public void sacar(int s){
+        if (this.getStatus()){
+            if(saldo > 0){
+                saldo = saldo - s;
+            } else {
+                System.out.println("Conta com saldo negativo");
+            }
+        } else {
+            System.out.println("Conta não aberta");
+        }
+    }
+    public void pagarMensalidade(){
+        int mensalidade = 0;
+        if (tipo.equals("CC")){
+            mensalidade = 12;
+        } else if (tipo.equals("CP")){
+            mensalidade = 20;
+        }
+        if (getStatus()){
+            if(saldo > 0){
+                saldo = saldo - mensalidade;
+            } else {
+                System.out.println("Conta com saldo negativo");
+            }
+        } else {
+            System.out.println("Conta não aberta");
+        }
+    }
+    // Métodos Especiais
+    public Conta(){
+        this.saldo = 0;
+        this.status = false;
+    }
     public void setConta(int c) {
         this.conta = c;
     }
@@ -36,58 +103,4 @@
     public boolean getStatus(){
         return status;
     }
-    public void abrirConta(String t){
-        setTipo(t);
-        setStatus(true);
-        if(t.equals("CC")){
-            setSaldo(50);
-        } else if(t.equals("CP")){
-            setSaldo(150);
-        }
-    }
-    public void fecharConta(int s){
-        if (saldo > 0){
-            System.out.println("Conta com dinheiro");
-        } else if (saldo < 0 ){
-            System.out.println("Conta com saldo negativo");
-        } else {
-            status = false;
-        }
-
-    }
-    public void depositar(int s){
-        if (status == true){
-            saldo = saldo + s;
-        } else if (status == false){
-            System.out.println("Conta não aberta");
-        }
-    }
-    public void sacar(int s){
-        if (status == true){
-            if(saldo > 0){
-                saldo = saldo - s;
-            } else {
-                System.out.println("Conta com saldo negativo");
-            }
-        } else if (status == false){
-            System.out.println("Conta não aberta");
-        }
-    }
-    public void pagarMensalidade(){
-        int mensalidade = 0;
-        if (tipo.equals("CC")){
-            mensalidade = 12;
-        } else if (tipo.equals("CP")){
-            mensalidade = 20;
-        }
-        if (status == true){
-            if(saldo > 0){
-                saldo = saldo - mensalidade;
-            } else {
-                System.out.println("Conta com saldo negativo");
-            }
-        } else {
-            System.out.println("Conta não aberta");
-        }
-    }
-}*/
+}
